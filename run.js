720c65d3032384b53484a75c2bca8a92876f9e22
Hannah Bunce <hannahlbunce@gmail.com>
2018-08-02 21:56:14 +0100

    Resolve #2

--- run.js
+++ run.js
@@ -1,13 +1,24 @@
 console.log("EVE Stats")
 const { fetch, write } = require('promise-path')
+const fs = require('fs')
 
-let promise = (async () => {
-  const apiContents = await fetch({
-    url: 'https://zkillboard.com/api/kills/regionID/12000001/page/1/',
-    headers: {
-      'User-Agent': `Cali Stats Bot`
-    }
-  })
-  await write('ora-page-one.json', JSON.stringify(JSON.parse(apiContents), null, 2))
-  console.log('Wrote data to file', apiContents.length,"bytes")
-})()
+async function run() {
+  //check to see if ora-page-one.json already exists
+  let filename = './ora-page-one.json'
+  if (fs.existsSync(filename)) {
+    console.log(`Skipping ${filename}`)
+  } else {
+    const apiContents = await fetch({
+      url: 'https://zkillboard.com/api/kills/regionID/12000001/page/1/',
+      headers: {
+        'User-Agent': `Cali Stats Bot`
+      }
+    })
+    await write('ora-page-one.json', JSON.stringify(JSON.parse(apiContents), null, 2))
+    console.log('Wrote data to file', apiContents.length, "bytes")
+  }
+
+
+}
+
+run()
