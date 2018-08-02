8dc2f52f8fe5a9a1f77fbee36a4d955a44836b46
Hannah Bunce <hannahlbunce@gmail.com>
2018-08-02 22:08:18 +0100

    Refactor to download based on a page number

--- run.js
+++ run.js
@@ -1,24 +1,27 @@
 console.log("EVE Stats")
 const { fetch, write } = require('promise-path')
 const fs = require('fs')
+const zeropad = require('./zeropad')
 
 async function run() {
+  return downloadPage(1)
+}
+
+async function downloadPage(number) {
   //check to see if ora-page-one.json already exists
-  let filename = './ora-page-one.json'
+  let filename = `./ora-page-${number}.json`
   if (fs.existsSync(filename)) {
     console.log(`Skipping ${filename}`)
   } else {
     const apiContents = await fetch({
-      url: 'https://zkillboard.com/api/kills/regionID/12000001/page/1/',
+      url: `https://zkillboard.com/api/kills/regionID/12000001/page/${number}/`,
       headers: {
         'User-Agent': `Cali Stats Bot`
       }
     })
-    await write('ora-page-one.json', JSON.stringify(JSON.parse(apiContents), null, 2))
+    await write(filename, JSON.stringify(JSON.parse(apiContents), null, 2))
     console.log('Wrote data to file', apiContents.length, "bytes")
   }
-
-
 }
 
 run()
