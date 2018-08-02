854dc42f98953e57b52b7cb5fa60fc9bd07200d3
Hannah Bunce <hannahlbunce@gmail.com>
2018-08-02 21:10:24 +0100

    Make request to zKillboard

--- run.js
+++ run.js
@@ -1,1 +1,12 @@
 console.log("EVE Stats")
+const { fetch } = require('promise-path')
+
+let promise = (async () => {
+  const apiContents = await fetch({
+    url: 'https://zkillboard.com/api/kills/regionID/12000001/page/1/',
+    headers: {
+      'User-Agent': `Cali Stats Bot`
+    }
+  })
+  console.log('Remote file:', apiContents)
+})()
